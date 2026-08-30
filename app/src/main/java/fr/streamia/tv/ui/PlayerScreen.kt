7a415ec8f133427c3f1529df0733a2926d63d23b
brahmiamine <ybrahmi66@gmail.com>
2026-08-30 20:56:19 +0200

    fix: opt in to Media3 PlayerView API

--- app/src/main/java/fr/streamia/tv/ui/PlayerScreen.kt
+++ app/src/main/java/fr/streamia/tv/ui/PlayerScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
@@ -58,6 +59,7 @@ import fr.streamia.tv.ui.theme.Night
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.yield
 
+@UnstableApi
 @Composable
 fun PlayerScreen(
     catalog: Catalog,
