ef42e032f3280c1e323bc6fd81ba48fa4b7aeaaf
brahmiamine <ybrahmi66@gmail.com>
2026-08-31 10:40:13 +0200

    chore: bump Streamia to 1.5.2 after conflict resolution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fr.streamia.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.5.1"
+        versionCode = 11
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
