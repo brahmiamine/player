8c4c1981ceacad5b379979b1e41e314815cc4c95
brahmiamine <ybrahmi66@gmail.com>
2026-08-31 13:38:21 +0200

    fix: import timeline geometry size

--- app/src/main/java/fr/streamia/tv/ui/PlayerScreen.kt
+++ app/src/main/java/fr/streamia/tv/ui/PlayerScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
